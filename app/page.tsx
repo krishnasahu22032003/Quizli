2e81114f5a1c1df51b61d0f3b57c669ee1061377
krishna sahu <krishna.sahu.work@gmail.com>
2026-05-31 01:28:37 +0530

    Landing page created

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import LandingPage from "@/pages/Landing";
+
+export default function Home() {
+  return (
+    <main className="bg-background text-foreground">
+      <LandingPage />
+    </main>
+  );
+}
---
+++ pages/Landing.tsx
@@ -0,0 +1,10 @@
+import Header from "@/components/ui/Header";
+
+
+export default function LandingPage() {
+  return (
+    <div className="relative min-h-screen overflow-hidden bg-background">
+      <Header />
+    </div>
+  );
+}
