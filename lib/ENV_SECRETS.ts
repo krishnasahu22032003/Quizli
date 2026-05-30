47077c1aad3246a8758ecfd67f137d08f4819c29
krishna sahu <krishna.sahu.work@gmail.com>
2026-05-30 23:33:21 +0530

    google key setup done

--- lib/ENV_SECRETS.ts
+++ lib/ENV_SECRETS.ts
@@ -2,7 +2,9 @@ import "dotenv/config" ;
 
 const ENV_SECRETS = {
 
-NEXTAUTH_SECRET:process.env.AUTH_SECRET
+NEXTAUTH_SECRET:process.env.AUTH_SECRET,
+Google_clientId:process.env.GOOGLE_CLIENT_ID   ,
+Google_clientSecret:process.env.GOOGLE_CLIENT_SECRET
 
 };
 
--- lib/auth.ts
+++ lib/auth.ts
@@ -1,23 +1,86 @@
-import { NextAuthOptions } from "next-auth";
+import { DefaultSession, NextAuthOptions } from "next-auth";
 import ENV_SECRETS from "./ENV_SECRETS";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import prisma from "@/app/lib/prisma";
+import GoogleProvider from "next-auth/providers/google";
 
-export const authOptions : NextAuthOptions = {
+declare module 'next-auth' {
 
-session:{
+  interface Session extends DefaultSession {
 
-  strategy:"jwt" 
+    user: {
+      id: string;
+    } & DefaultSession['user'];
+  };
+};
 
-},
+declare module 'next-auth/jwt' {
 
-secret:ENV_SECRETS.NEXTAUTH_SECRET ,
+  interface JWT {
 
-adapter:PrismaAdapter(prisma) ,
+    id: string
+
+  }
+
+}
 
 
+export const authOptions: NextAuthOptions = {
 
-providers:[]
+  session: {
+
+    strategy: "jwt"
+
+  },
+
+  callbacks: {
+
+    jwt: async ({ token }) => {
+
+      const user = await prisma.user.findFirst({
+        where: {
+          email: token?.email
+        }
+      })
+
+      if (user) {
+
+        token.id = user.id;
+
+      }
+
+      return token;
+    },
+
+    session: async ({ session, token }) => {
+
+      if (token) {
+
+        session.user.email = token.email
+        session.user.name = token.name
+        session.user.id = token.id
+
+      };
+
+      return session;
+    }
+
+  },
+
+secret: ENV_SECRETS.NEXTAUTH_SECRET,
+
+adapter: PrismaAdapter(prisma),
+
+providers:[
+
+  GoogleProvider({
+     clientId: ENV_SECRETS.Google_clientId as string , 
+    clientSecret:ENV_SECRETS.Google_clientSecret as string ,
+  })
+
+]
+
+
 
 
 
