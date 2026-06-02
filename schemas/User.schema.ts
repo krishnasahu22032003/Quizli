7b3ec8dc7c1c8e43f16ec8b09ac8a839ac92be1e
krishna sahu <krishna.sahu.work@gmail.com>
2026-06-02 19:43:38 +0530

    signin schema and update user schema added

--- schemas/User.schema.ts
+++ schemas/User.schema.ts
@@ -23,3 +23,32 @@ export const SignUpSchema = z.object({
     .transform((v) => v.trim()),
 });
 
+export const SignInSchema = z.object({
+  email: z
+    .string()
+    .email()
+    .min(5)
+    .max(50)
+    .transform((v) => v.trim().toLowerCase()),
+  password: z
+    .string()
+    .min(8)
+    .max(128)
+    .regex(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])/,
+      'Password must include uppercase, lowercase, number, and special character',
+    )
+    .transform((v) => v.trim()),
+});
+
+export const UpdateUserSchema =
+  SignUpSchema.partial().refine(
+    (data) =>
+      data.username ||
+      data.email ||
+      data.password,
+    {
+      message:
+        "At least one field is required",
+    }
+  );
