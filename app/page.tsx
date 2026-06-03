23fbb96490868a58153bacf61bd7970d8dc96934
krishna sahu <krishna.sahu.work@gmail.com>
2026-06-03 17:56:18 +0530

    middleware code addded

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,17 @@
 import LandingPage from "@/pages/Landing";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+import { authOptions } from "@/lib/auth";
 
-export default function Home() {
+export default async function Home() {
+
+  const session = await getServerSession(authOptions);
+
+  if (session) {
+
+    redirect("/dashboard")
+  };
+
   return (
     <main className="bg-background text-foreground">
       <LandingPage />
