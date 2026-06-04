43217e12c28afc4e10f5115ce5ba66e711c38cc8
krishna sahu <krishna.sahu.work@gmail.com>
2026-06-05 00:33:33 +0530

    quiz zod schema add

---
+++ schemas/Quiz.schema.ts
@@ -0,0 +1,21 @@
+import { z } from "zod";
+
+export const quizCreationSchema = z.object({
+  topic: z
+    .string()
+    .trim()
+    .transform((value) => value.replace(/\s+/g, " "))
+    .pipe(
+      z.string()
+        .min(4, "Topic must be at least 4 characters long")
+        .max(100, "Topic cannot exceed 100 characters")
+    ),
+
+  type: z.enum(["mcq", "open_ended"]),
+
+  amount: z.coerce
+    .number()
+    .int("Amount must be a whole number")
+    .min(1, "At least 1 question is required")
+    .max(20, "Maximum 20 questions allowed"),
+});
