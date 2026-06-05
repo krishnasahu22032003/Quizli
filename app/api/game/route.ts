89aa4df384b997b21a0af8b0d063246e7f25c205
krishna sahu <krishna.sahu.work@gmail.com>
2026-06-05 22:58:56 +0530

    game route code update

--- app/api/game/route.ts
+++ app/api/game/route.ts
@@ -1,70 +1,172 @@
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import {
-  getServerSession,
-  type NextAuthOptions,
-  type DefaultSession,
-} from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
 import prisma from "@/app/lib/prisma";
+import { getAuthSession } from "@/lib/auth";
+import { quizCreationSchema } from "@/schemas/Quiz.schema";
+import { NextResponse } from "next/server";
+import { z } from "zod";
+import axios from "axios";
 import ENV_SECRETS from "@/lib/ENV_SECRETS";
 
-declare module "next-auth" {
-  interface Session extends DefaultSession {
-    user: {
-      id: string;
-      // ...other properties
-      // role: UserRole;
-    } & DefaultSession["user"];
-  }
-
-  // interface User {
-  //   // ...other properties
-  //   // role: UserRole;
-  // }
-}
-
-declare module "next-auth/jwt" {
-  interface JWT {
-    id: string;
-  }
-}
-
-export const authOptions: NextAuthOptions = {
-  session: {
-    strategy: "jwt",
-  },
-  secret: ENV_SECRETS.NEXTAUTH_SECRET,
-  callbacks: {
-    jwt: async ({ token }) => {
-      const db_user = await prisma.user.findFirst({
-        where: {
-          email: token?.email,
+export async function POST(req: Request) {
+  try {
+    const session = await getAuthSession();
+    if (!session?.user) {
+      return NextResponse.json(
+        { error: "You must be logged in to create a game." },
+        {
+          status: 401,
+        }
+      );
+    }
+    const body = await req.json();
+    const { topic, type, amount } = quizCreationSchema.parse(body);
+    const game = await prisma.game.create({
+      data: {
+        gameType: type,
+        timeStarted: new Date(),
+        userId: session.user.id,
+        topic,
+      },
+    });
+    await prisma.topic_Count.upsert({
+      where: {
+        topic,
+      },
+      create: {
+        topic,
+        count: 1,
+      },
+      update: {
+        count: {
+          increment: 1,
         },
-      });
-      if (db_user) {
-        token.id = db_user.id;
-      }
-      return token;
-    },
-    session: ({ session, token }) => {
-      if (token) {
-        session.user.id = token.id;
-        session.user.name = token.name;
-        session.user.email = token.email;
-        session.user.image = token.picture;
-      }
-      return session;
-    },
-  },
-  adapter: PrismaAdapter(prisma),
-  providers: [
-    GoogleProvider({
-      clientId: ENV_SECRETS.Google_clientId as string,
-      clientSecret: ENV_SECRETS.Google_clientSecret as string,
-    }),
-  ],
-};
+      },
+    });
 
-export const getAuthSession = () => {
-  return getServerSession(authOptions);
-};
+    const { data } = await axios.post(
+      `${ENV_SECRETS.NEXTAUTH_SECRET as string}/api/questions`,
+      {
+        amount,
+        topic,
+        type,
+      }
+    );
+
+    if (type === "mcq") {
+      type mcqQuestion = {
+        question: string;
+        answer: string;
+        option1: string;
+        option2: string;
+        option3: string;
+      };
+
+      const manyData = data.questions.map((question: mcqQuestion) => {
+        // mix up the options lol
+        const options = [
+          question.option1,
+          question.option2,
+          question.option3,
+          question.answer,
+        ].sort(() => Math.random() - 0.5);
+        return {
+          question: question.question,
+          answer: question.answer,
+          options: JSON.stringify(options),
+          gameId: game.id,
+          questionType: "mcq",
+        };
+      });
+
+      await prisma.questions.createMany({
+        data: manyData,
+      });
+    } else if (type === "open_ended") {
+      type openQuestion = {
+        question: string;
+        answer: string;
+      };
+      await prisma.questions.createMany({
+        data: data.questions.map((question: openQuestion) => {
+          return {
+            question: question.question,
+            answer: question.answer,
+            gameId: game.id,
+            questionType: "open_ended",
+          };
+        }),
+      });
+    }
+
+    return NextResponse.json({ gameId: game.id }, { status: 200 });
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      return NextResponse.json(
+        { error: error.issues },
+        {
+          status: 400,
+        }
+      );
+    } else {
+      return NextResponse.json(
+        { error: "An unexpected error occurred." },
+        {
+          status: 500,
+        }
+      );
+    }
+  }
+}
+export async function GET(req: Request) {
+  try {
+    const session = await getAuthSession();
+    if (!session?.user) {
+      return NextResponse.json(
+        { error: "You must be logged in to create a game." },
+        {
+          status: 401,
+        }
+      );
+    }
+    const url = new URL(req.url);
+    const gameId = url.searchParams.get("gameId");
+    if (!gameId) {
+      return NextResponse.json(
+        { error: "You must provide a game id." },
+        {
+          status: 400,
+        }
+      );
+    }
+
+    const game = await prisma.game.findUnique({
+      where: {
+        id: gameId,
+      },
+      include: {
+        questions: true,
+      },
+    });
+    if (!game) {
+      return NextResponse.json(
+        { error: "Game not found." },
+        {
+          status: 404,
+        }
+      );
+    }
+
+    return NextResponse.json(
+      { game },
+      {
+        status: 200,
+      }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { error: "An unexpected error occurred." },
+      {
+        status: 500,
+      }
+    );
+  }
+}
