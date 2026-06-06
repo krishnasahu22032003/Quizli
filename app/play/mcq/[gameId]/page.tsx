d2125442804e7c5791274426133ddf153fc0d567
krishna sahu <krishna.sahu.work@gmail.com>
2026-06-06 22:17:49 +0530

    mcq page code done

---
+++ app/play/mcq/[gameId]/page.tsx
@@ -0,0 +1,38 @@
+import MCQ from "@/components/MCQ";
+import prisma from "@/app/lib/prisma";
+import { getAuthSession } from "@/lib/auth";
+import { redirect } from "next/navigation";
+
+type Props = {
+  params: {
+    gameId: string;
+  };
+};
+
+const MCQPage = async ({ params: { gameId } }: Props) => {
+  const session = await getAuthSession();
+  if (!session?.user) {
+    return redirect("/");
+  }
+
+  const game = await prisma.game.findUnique({
+    where: {
+      id: gameId,
+    },
+    include: {
+      questions: {
+        select: {
+          id: true,
+          question: true,
+          options: true,
+        },
+      },
+    },
+  });
+  if (!game || game.gameType === "open_ended") {
+    return redirect("/quiz");
+  }
+  return <MCQ game={game} />;
+};
+
+export default MCQPage;
