16603a008f738396bfacaf2c4b288e25cdf5e3c3
krishna sahu <krishna.sahu.work@gmail.com>
2026-06-06 22:11:59 +0530

    open-ended page.tsx done

---
+++ app/play/open-ended/[gameId]/page.tsx
@@ -0,0 +1,41 @@
+import OpenEnded from "@/components/OpenEnded";
+import prisma from "@/app/lib/prisma";
+import { getAuthSession } from "@/lib/auth";
+import { redirect } from "next/navigation";
+import React from "react";
+
+type Props = {
+    params: {
+        gameId: string;
+    };
+};
+
+const OpenEndedPage = async ({ params: { gameId } }: Props) => {
+    const session = await getAuthSession();
+    if (!session?.user) {
+        return redirect("/");
+    }
+
+    const game = await prisma.game.findUnique({
+        where: {
+            id: gameId,
+        },
+        include: {
+            questions: {
+                select: {
+                    id: true,
+                    question: true,
+                    answer: true,
+                },
+            },
+        },
+    });
+
+    if (!game || game.gameType === "mcq") {
+        return redirect("/quiz");
+    };
+    
+    return <OpenEnded game={game} />;
+};
+
+export default OpenEndedPage;
